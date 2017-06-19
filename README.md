36f70846ee812f33ea566a9dfeb419a1748bb5c7
Nathaniel <fna371858@gmail.com>
2017-06-19 20:03:12 -0300

    Main Upload
    Test it OUT!

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-#### This plugin is going to be uploaded tonight, please stand by.
+## Tweeter
+> This plugin was developed by @NFGamerMC with some great contributors such as @Matthww
+------
+> We have a **PRO** version that removes the *"- Sent by @TweeterPlugin"* watermark and ads, get it [here](http://sellfy.com/NFGamerMC) for just 5$ 
+
+| CodeShip   |      CircleCi      |  Poggit | 
+|----------|:-------------:|------|
+| [![Codeship](https://img.shields.io/badge/build-unknown-red.svg?style=plastic)]() |  ![CirlceCI](https://img.shields.io/badge/build-unknown-red.svg?style=plastic) | ![Poggit-CI](https://poggit.pmmp.io/ci.shield/PluginStream/Tweetr/Tweetr) |
+
+## Information
+**Demo Account:**<br>
+We have a demo account, this is the default place that this plugin will tweet too,
+you can change it to your account in settings. You can [go here](https://twitter.com/TweeterPlugin) if you want too see what other people have used this for. **Warning: That account is not monitored, their may be NSFW content. This is be contained hopefully. You must be mature to view the account. That means nobody under 13 years of age. 15+ reccommended**<br>
+**Docs**<br>
+We are starting to develop an API, this will be documented here.
+The source code can be found in the orgin\docs branch.<br>
+**Privacy**<br>
+We are very passionate about user safety, [this is a link](https://github.com/PluginStream/Tweetr/wiki/Privacy)
+to help you keep your account safe from people.<br>
+**Features**<br>
+- Get a user's tweets
+- DM All Followers for upcoming Events
+- Send a Tweet (140C)
+- Delete Latest Tweet (Coming Soon...)
+- Add image to tweets by default (Coming Soon...)
+
+## Installation
+**Self-installation:**<br>
+Check out this the wiki if you would like to install!
+Supported platforms: Linux, Windows, OS X, Raspberry Pi, and ODROID
+
+**Installation on a Hosting Company:**<br>
+We do not provide any support when using a hosting company, it will be your
+job to install the plugin. We will help you debug and fix things though.
+
+* __[HostMyServers](https://hostmyservers.com)__
+* __[Host-MCPE](http://host-mcpe.us)__
+* __[MinersPlanet](http://minersplanet.com)__
+* __[HK-Hosting GbR](https://hk-hosting.de)__
+* __[Virtual Gladiators](http://virtualgladiators.com)__
+* __[NetherBox](https://netherbox.com/?promo=IMAGICAL)__
+* __[ExtraVM](https://www.extravm.com/minecraft.html)__
+* __[24serv.pro](https://24serv.pro)__ 
+* __[ServerWays Hosting](https://serverwayshosting.com)__
+
+### External Links
+* __[Homepage](http://nfgamermc.com)__
+* __[Google+](https://plus.google.com/114615579347555913881)__
+* __[Keybase](https://keybase.io/NFGamerMC)__
+* __[Twitter](http://twitter.com/NFGamerMC)__
+* __[PluginStream Twitter](http://twitter.com/PluginStream)__
+* __[Discord](http://bit.ly/2r7sxsg)__
+* __[XBOX Live](https://account.xbox.com/en-CA/Profile?GamerTag=NFGamerMC)__
+
+### Disclaimer
+This plugin is in no way affilliated with AppleDevelops nor any of his Partners. This is a one man project by @NFGamerMC! We will close and ban the ones that open issues about phars. We can provide support a the PluginStream Twitter. 
---
+++ src/NFGamerMC/Base.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace NFGamerMC;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\Player;
+use NFGamerMC\TwitterAPI\TwitterAPIExchange;
+
+class Base extends PluginBase implements Listener{
+    public function onLoad(){
+        $this->getLogger()->info("Plugin Loading");
+    }
+    public function onEnable()
+    {
+        $this->saveDefaultConfig();
+        $this->reloadConfig();
+        $settings = array(
+            'oauth_access_token' => $this->getConfig()->get("access-token"),
+            'oauth_access_token_secret' => $this->getConfig()->get("access-secret"),
+            'consumer_key' => $this->getConfig()->get("consumer-key"),
+            'consumer_secret' => $this->getConfig()->get("consumer-secret")
+        );
+        $url = 'https://api.twitter.com/1.1/statuses/update.json';
+        $requestMethod = 'POST';
+        $postfields = array(
+            'status' => $this->getConfig()->get("startup") . ' - Sent using @TweeterPlugin',
+        );
+        $twitter = new TwitterAPIExchange($settings);
+        echo $twitter->buildOauth($url, $requestMethod)->setPostfields($postfields)->performRequest(true, [
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false
+        ]);
+    }
+    public function onDisable(){
+        $this->getLogger()->info("Plugin Disabled");
+        $settings = array(
+            'oauth_access_token' => $this->getConfig()->get("access-token"),
+            'oauth_access_token_secret' => $this->getConfig()->get("access-secret"),
+            'consumer_key' => $this->getConfig()->get("consumer-key"),
+            'consumer_secret' => $this->getConfig()->get("consumer-secret")
+        );
+        $url = 'https://api.twitter.com/1.1/statuses/update.json';
+        $requestMethod = 'POST';
+        $msg = (implode(" ", array_slice($args, 0)));
+        $postfields = array(
+            'status' => $this->getConfig()->get("shutdown") . ' - Sent using @TweeterPlugin',
+        );
+        $twitter = new TwitterAPIExchange($settings);
+        echo $twitter->buildOauth($url, $requestMethod)->setPostfields($postfields)->performRequest(true, [
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false
+        ]);
+    }
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args)
+    {
+        switch (strtolower($command->getName())) {
+            case "post":
+                if ($sender instanceof Player) {
+                    $settings = array(
+                        'oauth_access_token' => $this->getConfig()->get("access-token"),
+                        'oauth_access_token_secret' => $this->getConfig()->get("access-secret"),
+                        'consumer_key' => $this->getConfig()->get("consumer-key"),
+                        'consumer_secret' => $this->getConfig()->get("consumer-secret")
+                    );
+                    $url = 'https://api.twitter.com/1.1/statuses/update.json';
+                    $requestMethod = 'POST';
+                    $msg = (implode(" ", array_slice($args, 0)));
+                    $postfields = array(
+                        'status' => $msg . ' - Sent using @TweeterPlugin',
+                    );
+                    $twitter = new TwitterAPIExchange($settings);
+                    echo $twitter->buildOauth($url, $requestMethod)->setPostfields($postfields)->performRequest(true, [
+                        CURLOPT_SSL_VERIFYHOST => false,
+                        CURLOPT_SSL_VERIFYPEER => false
+                    ]);
+                    $sender->sendMessage(C::AQUA . C::BOLD . "Twitter: " . C::RESET . C::GRAY . "Sent " . $msg);
+                    break;
+                } else {
+                    $sender->sendMessage(C::DARK_RED . "Please run this command IN-GAME!");
+                }
+                return true;
+            case "get":
+                if ($sender instanceof Player) {
+                    $settings = array(
+                        'oauth_access_token' => $this->getConfig()->get("access-token"),
+                        'oauth_access_token_secret' => $this->getConfig()->get("access-secret"),
+                        'consumer_key' => $this->getConfig()->get("consumer-key"),
+                        'consumer_secret' => $this->getConfig()->get("consumer-secret")
+                    );
+                    $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+                    $requestMethod = "GET";
+                    $user = (implode(" ", array_slice($args, 0)));
+                    $count = (implode(" ", array_slice($args, 1)));
+                    $getfield = '?screen_name=' . $user . '&count=' . $count;
+                    $twitter = new TwitterAPIExchange($settings);
+                    $response = $twitter->setGetfield($getfield)
+                        ->buildOauth($url, $requestMethod)
+                        ->performRequest(true, [
+                            CURLOPT_SSL_VERIFYHOST => false,
+                            CURLOPT_SSL_VERIFYPEER => false
+                        ]);
+                    $tweets = json_decode($response);
+
+                    foreach ($tweets as $tweet) {
+                        $sender->sendMessage(C::AQUA . C::BOLD . "Twitter:" . C::RESET . " " . $tweet->text);
+                    }
+                } else {
+                    $sender->sendMessage(C::DARK_RED . "Please run this command IN-GAME!");
+                }
+                return true;
+            case "dm":
+                if ($sender instanceof Player) {
+                    $settings = array(
+                        'oauth_access_token' => $this->getConfig()->get("access-token"),
+                        'oauth_access_token_secret' => $this->getConfig()->get("access-secret"),
+                        'consumer_key' => $this->getConfig()->get("consumer-key"),
+                        'consumer_secret' => $this->getConfig()->get("consumer-secret")
+                    );
+                    $url = 'https://api.twitter.com/1.1/direct_messages/new.json';
+                    $requestMethod = 'POST';
+                    $user = (implode(" ", array_slice($args, 0)));
+                    $msg = (implode(" ", array_slice($args, 1)));
+                    $postfields = array(
+                        'screen_name' => $args[0],
+                        'text' => $msg . ' - Sent using @TweeterPlugin',
+                    );
+                    $twitter = new TwitterAPIExchange($settings);
+                    echo $twitter->buildOauth($url, $requestMethod)->setPostfields($postfields)->performRequest(true, [
+                        CURLOPT_SSL_VERIFYHOST => false,
+                        CURLOPT_SSL_VERIFYPEER => false
+                    ]);
+                } else {
+                    $sender->sendMessage("Please run this command IN-GAME!");
+                }
+                return true;
+        }
+    }
+}
